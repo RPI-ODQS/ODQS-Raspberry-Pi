7fd6080f2ab374e7fc02277e72498b4cec3ef6f8
David-Tan-YiFan <704778682@qq.com>
2018-03-31 22:24:19 +0800

    bug fix

--- project/Install/install.py
+++ project/Install/install.py
@@ -7,6 +7,6 @@ os.system("sudo apt-get install python-setuptools")
 os.system("sudo easy_install pip")
 
 os.system("sudo pip install -U ubidots==1.6.6")
-os.system("sudo pip install paho-mqtt")
+os.system("sudo pip3 install paho-mqtt")
 
 print ("Instalation finished")
